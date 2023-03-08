788847a0c2a8bd913320a3e974db30f33709f7d5
Rifky Ramadhan <rifky.r3@gmail.com>
2023-03-08 10:34:24 +0700

    dynamic run php

--- PHP/RUN/php54-apache.ps1
+++ PHP/RUN/php54-apache.ps1
@@ -1,7 +1,15 @@
-docker run -d --add-host host.docker.internal:host-gateway `
-    -p 8054:80 `
-    --name php54-apache `
-    -v D:\Program\web\www:/var/www/html `
-    -v D:\Program\docker\data\php-apache\54\conf\conf-old.ini:/usr/local/etc/php/conf.d/99optional.ini `
-    -v D:\Program\docker\data\php-apache\54\log:/usr/local/etc/php/log `
-    chocin/php:5.4-apache-xdebug
+######## CONFIG ########################################
+$name = '--name=php54-apache'
+$port = '-p=8054:80'
+$webDir = 'D:\Program\web\www'
+$image = 'chocin/php:5.4-apache-xdebug'
+########################################################
+
+$params = 'run', '-d', '--add-host=host.docker.internal:host-gateway'
+$params += $name, $port
+$params += "-v=$($webDir):/var/www/html"
+$params += "-v=$(Resolve-Path (Join-Path (get-location).path '..'))\conf-old.ini:/usr/local/etc/php/conf.d/99optional.ini"
+$params += $image
+
+& 'docker.exe' @params
+# echo 'docker.exe' @params
