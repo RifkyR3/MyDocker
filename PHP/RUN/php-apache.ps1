45c6178104bd7aefabe188b758b1680b4b283fbd
Rifky Ramadhan <rifky.r3@gmail.com>
2023-03-08 10:50:01 +0700

    update run php

--- PHP/RUN/php-apache.ps1
+++ PHP/RUN/php-apache.ps1
@@ -8,8 +8,12 @@ $image = 'chocin/php:8.2-apache-xdebug'
 $params = 'run', '-d', '--add-host=host.docker.internal:host-gateway'
 $params += $name, $port
 $params += "-v=$($webDir):/var/www/html"
-$params += "-v=$(Resolve-Path (Join-Path (get-location).path '..'))\conf-old.ini:/usr/local/etc/php/conf.d/99optional.ini"
-$params += $image
+## optional config
+# config php 5
+# $params += "-v=$(Resolve-Path (Join-Path (get-location).path '..'))\conf-old.ini:/usr/local/etc/php/conf.d/99optional.ini"
+# config php 7^
+$params += "-v=$(Resolve-Path (Join-Path (get-location).path '..'))\conf-new.ini:/usr/local/etc/php/conf.d/99optional.ini"
 
+$params += $image
 & 'docker.exe' @params
 # echo 'docker.exe' @params
